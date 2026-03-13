b4362ca59b09f7b781a17c4a49ac63cc745d1393
AbhinaySinghGaharwar <abhinaysinghgaharwar999@gmail.com>
2026-03-13 19:11:15 +0530

    Added Whatsapp Button At Bottom Right

--- src/app/components/LayoutWrapper.jsx
+++ src/app/components/LayoutWrapper.jsx
@@ -3,6 +3,7 @@
 import { usePathname } from "next/navigation";
 import Navbar from "./Header";
 import Footer from "./Footer";
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function LayoutWrapper({ children, logo, siteName }) {
 
@@ -13,11 +14,31 @@ export default function LayoutWrapper({ children, logo, siteName }) {
   const hide =
     pathname.startsWith("/user") || pathname.startsWith("/admin");
 
+
+   const openWhatsApp = () => {
+    window.open(`https://api.whatsapp.com/send?phone=917489174685&text=Hello%20InstantSMMBoost`, "_blank");
+  };
   return (
-    <>
+    <div className="relative">
       {!hide && <Navbar logo={logo} siteName={siteName} />}
       {children}
+      <button
+        onClick={openWhatsApp}
+        className="
+          px-3 py-3 text-sm
+          sm:px-4 sm:py-4 sm:text-base
+          lg:px-4 lg:py-4 lg:text-lg
+          rounded-full font-semibold fixed bottom-5 right-5 z-51
+          bg-green-500/30 hover:bg-green-500/50
+          text-green-700 dark:text-green-400
+          dark:bg-green-500/40 dark:hover:bg-green-500/20
+          backdrop-blur transition shadow-sm whitespace-nowrap
+          disabled:opacity-50
+        "
+      >
+        <FaWhatsapp className="text-lg sm:text-xl lg:text-2xl" />
+      </button>
       {!hide && <Footer siteName={siteName} />}
-    </>
+    </div>
   );
 }
